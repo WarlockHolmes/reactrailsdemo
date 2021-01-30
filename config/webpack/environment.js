ec4411ac988baf0ad855b9b16579b65512f85280
WarlockHolmes <grau.morgan@gmail.com>
2021-01-30 13:15:43 -0700

    heroku

---
+++ config/webpack/environment.js
@@ -0,0 +1,17 @@
+// environment.js
+const { environment } = require('@rails/webpacker')
+
+const path = require('path')
+
+const customConfig = {
+  resolve: {
+    alias: {
+      '@src': path.resolve(__dirname, '..', '..', 'app/javascript/src'),
+    }
+  }
+}
+
+environment.config.merge(customConfig);
+// enable split chunk with default settings with following line
+environment.splitChunks()
+module.exports = environment
